472b81597f849af69d758d3d3b643e862d6f5a2b
Anurag El Dorado <annurag94@gmail.com>
2016-04-13 22:24:31 +0530

    Draggable & links done.

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ $(document).keydown(function(e) {
 	            var range = selRange.cloneRange();
 	            range.selectNodeContents(blockEl);
 	            range.setStart(selRange.endContainer, selRange.endOffset);
-	            return range.extractContents();
+	            return range.extractContents().textContent;
 	        }
 	    }
 	}
@@ -64,8 +64,12 @@ $(document).keydown(function(e) {
 	if (focusOn) {	
 		if (e.keyCode == 13) {	// code for enter key
 			e.preventDefault();
-			var text = (extractContentsFromCaret().textContent);
-			
+			var text = (extractContentsFromCaret());
+			// var text = (extractContentsFromCaret().textContent);
+			text = text.split('<').join('&lt;');
+			text = text.split('>').join('&gt;');
+			console.log(text);
+
 			if (text.charAt(0) == ' ') {
 				text = text.substring(1);
 			}
@@ -78,6 +82,7 @@ $(document).keydown(function(e) {
 			$('.added-last').on('focus', pfocus);
 			$('.added-last').on('blur', pblur);
 			$('.added-last').focus();
+			$('.added-last').on('dblclick', showTooltip);
 			// $('.added-last').text('');
 		}
 
@@ -160,6 +165,7 @@ $(document).keydown(function(e) {
 
 $('.added-last').on('focus', pfocus);
 $('.added-last').on('blur', pblur);
+$('.added-last').on('dblclick', showTooltip);
 
 function pfocus() {
 	isfocus = true;
@@ -175,22 +181,119 @@ function caretPosition() {
     caretPos = (window.getSelection().getRangeAt(0).startOffset);
 }
 
-document.getElementById('checkEditable').addEventListener('click', function() { 
+document.getElementById('check-editable').addEventListener('click', function() { 
 	var allParagraphs = document.querySelectorAll('p');
 	if (this.checked) {
-		for (var i = 0; i < allParagraphs.length; i++) {
-			allParagraphs[i].setAttribute('contenteditable', true);
-			allParagraphs[i].setAttribute('draggable', false);
-		}
+
+		clearAllLinks();
+
+		[].forEach.call(allParagraphs, function(para) {
+			para.setAttribute('contenteditable', true);
+			para.setAttribute('draggable', false);
+		});
 	} else {
-		for (var i = 0; i < allParagraphs.length; i++) {
-			allParagraphs[i].setAttribute('contenteditable', false);
-			allParagraphs[i].setAttribute('draggable', true);
-		}
+
+		consolidateLinks();
+
+		[].forEach.call(allParagraphs, function(para) {
+			para.setAttribute('contenteditable', false);
+
+			para.setAttribute('draggable', true);
+  			para.addEventListener('dragstart', handleDragStart, false);
+  			para.addEventListener('dragenter', handleDragEnter, false);
+  			para.addEventListener('dragover', handleDragOver, false);
+  			para.addEventListener('dragleave', handleDragLeave, false);
+  			para.addEventListener('drop', handleDrop, false);
+			para.addEventListener('dragend', handleDragEnd, false);
+		});
 	}
 }, false);
 
 
+var dragSrc = null;
+function handleDragStart(e) {
+	this.style.opacity = 0.4;
+	dragSrc = this;
+	console.log(dragSrc);
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+	if (e.preventDefault) {
+		e.preventDefault(); // Necessary. Allows us to drop.
+	}
+	e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+	return false;
+}
+
+function handleDragEnter(e) {
+	this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+	this.classList.remove('over');
+}
+
+function handleDrop(e) {
+	// this / e.target is current target element.
+	if (e.stopPropagation) {
+		e.stopPropagation(); // stops the browser from redirecting.
+	}
+
+	// Don't do anything if dropping the same column we're dragging.
+	if (dragSrc != this) {
+		// Set the source column's HTML to the HTML of the column we dropped on.
+		dragSrc.innerHTML = this.innerHTML;
+		this.innerHTML = e.dataTransfer.getData('text/html');
+	}
+
+	return false;
+}
+
+function handleDragEnd(e) {
+	// this/e.target is the source node.
+	var allParagraphs = document.querySelectorAll('p');
+	[].forEach.call(allParagraphs, function (para) {
+		para.classList.remove('over');
+		para.style.opacity = 1;
+	});
+}
+
+
+
+var tooltip = document.getElementById('tooltip');
+var selToChange = null;
+function showTooltip(e) {
+	tooltip.style.left = e.pageX;
+	tooltip.style.top = e.pageY;
+	tooltip.style.display = 'block';
+	selToChange = window.getSelection();
+	console.log(selToChange);
+}
+
+var tooltipOptions = document.querySelectorAll('.tooltip-option');
+[].forEach.call(tooltipOptions, function(option) {
+	option.addEventListener('click', function(e) {
+		// e.preventDefault();
+		console.log(selToChange);
+		console.log(e.target.id);
+		document.execCommand('bold');
+
+		tooltip.style.display = 'none';
+	}, false);
+});
+
+
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
 
 /*
 
@@ -200,3 +303,35 @@ function setColor() {
 }
 
  */
+
+var linksdiv = document.getElementById('links');
+function consolidateLinks() {
+	var allParagraphs = document.querySelectorAll('p');
+	var content = '';
+	[].forEach.call(allParagraphs, function(para) {
+		content += para.innerHTML;
+	});
+
+	var links = content.match(/&lt;a&gt;.*?&lt;\/a&gt;/g) || [];
+	[].forEach.call(links, function(link) {
+		link = link.replace(/&lt;a&gt;/g, '');
+		link = link.replace(/&lt;\/a&gt;/g, '');
+
+		var newdiv = document.createElement('div');
+		var newlink = document.createElement('a');
+		newlink.appendChild(document.createTextNode(link));
+		newdiv.appendChild(newlink);
+		linksdiv.appendChild(newdiv);
+		// console.log(link);
+	});
+	// for (i = 0; i < content.length; i++) {
+		// console.log(content.charAt(i));
+	// }
+
+}
+
+function clearAllLinks() {
+	while (linksdiv.firstChild) {
+	    linksdiv.removeChild(linksdiv.firstChild);
+	}
+}
